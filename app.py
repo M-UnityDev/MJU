6f9cc342a2d061fc61c5b922169eabfa6d21f9be
M-Unity <mihailsheshukov10@gmail.com>
2025-07-28 19:49:00 +0500

    second commit

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def upload_file():
         return 'File uploaded successfully', 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5005)
+    app.run(debug=False, host='0.0.0.0', port=5005)
